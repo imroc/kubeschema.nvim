144d136e0f9e61193da152ad172c8018e02053eb
roc <roc@imroc.cc>
2024-12-23 14:56:19 +0800

    add setup

---
+++ lua/kubernetes/init.lua
@@ -0,0 +1,20 @@
+local M = {}
+
+---@class kubernetes.Config
+---@field cache_dir? string
+local config = {
+	cache_dir = vim.fn.stdpath("data") .. "/kubenretes",
+}
+
+M.did_setup = false
+
+---@param opts kubernetes.Config?
+function M.setup(opts)
+	if M.did_setup then
+		return vim.notify("kubernetes.nvim is already setup", vim.log.levels.ERROR, { title = "kubernetes.nvim" })
+	end
+	M.did_setup = true
+	config = vim.tbl_deep_extend("force", config, opts or {})
+end
+
+return M
